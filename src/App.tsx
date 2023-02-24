c7df57b8c71c4d592c73f07307583d68259b1084
Cry-pr <cristian.pirlea@iis.utm.md>
2023-02-24 14:34:05 +0200

    First commit for lab2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { observer } from 'mobx-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App: React.FC=observer(()=>{
+return (
+  <section>
+    <div className='box'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <div className='content'>
+        <img src={logo} alt="Logo" className='App-logo' />
+        <h2>My personal Data</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <b >First Name: Pirlea <br />
+          Last Name: Cristian <br />
+          Grouo: Cr-212</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
     </div>
-  );
-}
+  </section>
+);}
+)
 
 export default App;
